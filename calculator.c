ec065e38253ce6213d02bbef88e9bf77bbbd2550
hariharan shanmugam <harideeshan007@gmail.com>
2024-07-21 13:33:17 +0530

    updated calculator using switch case

--- calculator.c
+++ calculator.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     char symbol;
-    int a,b;
-    printf("enter the operation to done(+, _, *, /,%%):\n");
-    scanf("%c",&symbol);
+    int a, b;
+    printf("enter the operation to done(+, -, *, /,%%):\n");
+    scanf("%c", &symbol);
     printf("enter two numbers:\n");
-    scanf("%d%d",&a ,&b);
+    scanf("%d%d", &a, &b);
 
-    switch (symbol){
-        case '+':
-            printf("result of addition is :%d\n", (a+b));
-            break;
-        case '-':
-            printf("result of subtraction is :%d\n", (a-b));
-            break;
-        case '*':
-            printf("result of multiplication is :%d\n", (a*b));
-            break;
-        case '/':
-            printf("result of division is :%d\n", (a/b));
-            break;
-        case '%':
-            printf("result of remainder is :%d\n", (a%b));
-            break;
-        default:
-            printf("error...Try Again");
-            break;
+    switch (symbol)
+    {
+    case '+':
+        printf("result of addition is :%d\n", (a + b));
+        break;
+    case '-':
+        printf("result of subtraction is :%d\n", (a - b));
+        break;
+    case '*':
+        printf("result of multiplication is :%d\n", (a * b));
+        break;
+    case '/':
+        if (b == 0)
+            printf("you cannot divide by 0\n");
+        else
+            printf("result of division is :%d\n", (a / b));
+        break;
+    case '%':
+        if (b == 0)
+            printf("you cannot divide by 0\n");
+        else
+            printf("result of remainder is :%d\n", (a % b));
+        break;
+    default:
+        printf("error...Try Again");
+        break;
     }
-    
-
-
-
-
-
-
-
-
-
 }
