b558e2567aac20a668a6137e7e3242c855956861
hariharan shanmugam <harideeshan007@gmail.com>
2024-07-30 07:33:44 +0530

    for loop

---
+++ For_loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    int i;
+
+    printf("enter the number:\n");
+    scanf("%d", &num);
+
+    for(i = 0;i < num;i++){
+        printf("hello\n");
+    }
+}
