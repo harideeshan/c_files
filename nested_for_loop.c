b4693e245c0169dcb783d5c0ea584e95253861cd
hariharan shanmugam <harideeshan007@gmail.com>
2024-08-01 15:12:09 +0530

    nested for loop

---
+++ nested_for_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num = 4;
+
+    for(int i = 1;i < num;i++){
+        for(int j = 0; j< num;j++){
+            printf(" i = %d, j = %d\n",i , j);
+             
+
+        }
+    }
+}
